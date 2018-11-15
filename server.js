e35ba7a1ed39cfb84fe1fd75b1ff6f2decbf10d8
Ivan Zalessov <ivan.zalessov@gmail.com>
2018-11-15 16:01:57 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const port = 3000;
+
+// Handles HTTP requests.
+const requestHandler = (request, response) => {
+  response.end(`Handling a request on port ${port}`)
+};
+
+// Create a server and pass in the requestHandler function
+const server = http.createServer(requestHandler);
+
+// Start the server listening on port 8000
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error:  ${err}`);
+  }
+  let fs = require('fs');
+  fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
+    if (err) throw err;
+
+    console.log('Wrote to file hello-world.txt')
+  });
+});
